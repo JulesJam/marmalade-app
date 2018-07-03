bf0ae2496bc32400f941d8b98b3051d9c7f0cee8
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2018-07-03 15:10:35 +0100

    css bugs fixed now seems ok for responsiveness use emulator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,29 +11,12 @@ export class AboutComponent implements OnInit {
 
   
   constructor() {
-    this.location = "";
+ }
   
-   }
 
-  findMe() {
-    alert("Looking for you...");
-      if (navigator.geolocation) {
-        console.log("looking now...");
-        navigator.geolocation.getCurrentPosition((position) => {
-          let currentLat = position.coords.latitude.toString()
-          let currentLong = position.coords.longitude.toString()
-          this.location = "Your latitude : " + currentLat + " longitude: " + currentLong;
-          console.log("Position is ",position.coords.latitude, position.coords.longitude);
-          console.log("is anything happening");
-        });
-      } else {
-        alert("Geolocation is not supported by this browser.");
-      }
-    }
 
   ngOnInit() {
-    
-    this.findMe()
+ 
 
 
   }
