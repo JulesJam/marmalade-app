6884a9bb1de20512cca8351ad32de3976453ee2c
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2018-08-23 14:52:23 +0100

    added direction calculator need to implement in html etc

--- src/app/distance-calculator.service.ts
+++ src/app/distance-calculator.service.ts
@@ -24,7 +24,31 @@ export class DistanceCalculatorService {
     } else if (d < 10){
       d = (Math.round(d * 100)) / 100;
     }
+    this.direction(lat1, lon1, lat2, lon2);
     return d ;
+  }
+
+  private  direction(lat1, lon1, lat2, lon2) {
+    let x1 = lat1;
+    let y1 = lon1;
+    let x2 = lat2;
+    let y2 = lon2;
+
+    var radians = getAtan2((y1 - y2), (x1 - x2));
+
+    function getAtan2(y, x) {
+        return Math.atan2(y, x);
+    };
+
+    var compassReading = radians * (180 / Math.PI);
+
+    var coordNames = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"];
+    var coordIndex = Math.round(compassReading / 45);
+    if (coordIndex < 0) {
+        coordIndex = coordIndex + 8
+    };
+    console.log("direction",compassReading, coordNames[coordIndex])
+    return coordNames[coordIndex]; // returns the coordinate value
   }
 
   private deg2rad(deg): number {
